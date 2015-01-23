aebec5119a900b588b87c25bee29fd818bf157dc
Aria Stewart <aredridel@dinhe.net>
2015-01-23 17:56:42 -0500

    Handle both paypal-style and bcp47-style locale objects

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ var iferr = require('iferr');
 module.exports = function(dust, options) {
     options = options || {};
 
-    usecontent(function(locality, bundle, cb) {
-        if (!locality.country ) return cb(new Error("no country present"));
-        if (!locality.language) return cb(new Error("no language present"));
-        spundle(options.localeRoot, locality.country, locality.language, iferr(cb, function (messages) {
-            cb(null, messages[[locality.language, locality.country].join('-')][bundle]);
+    usecontent(function(locale, bundle, cb) {
+        /* Handle paypal-style or bcp47-style objects */
+        var country = locale.country || locale.langtag.region;
+        var language = locale.language || locale.langtag.language.language;
+        if (!country ) return cb(new Error("no country present"));
+        if (!language) return cb(new Error("no language present"));
+        spundle(options.localeRoot, country, language, iferr(cb, function (messages) {
+            cb(null, messages[[language, country].join('-')][bundle]);
         }));
     }).registerWith(dust);
     message.registerWith(dust);
