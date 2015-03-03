42cb929282b21291f6452f5101e92ac5a1d12111
Aria Stewart <aredridel@dinhe.net>
2015-03-03 08:23:30 -0500

    Add support for default content loaded for each template

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
+"use strict";
 var usecontent = require('dust-usecontent-helper');
 var message = require('dust-message-helper');
 var spundle = require('spundle');
+var dustjacket = require('dustjacket');
 var iferr = require('iferr');
 
 module.exports = function(dust, options) {
     options = options || {};
 
+    dustjacket.registerWith(dust);
+
+    dust.addLoadMiddleware(defaultContent);
+
     usecontent(function(locale, bundle, cb) {
         /* Handle paypal-style or bcp47-style objects */
         var country = locale.country || locale.langtag.region;
         var language = locale.language || locale.langtag.language.language;
-        if (!country ) return cb(new Error("no country present"));
-        if (!language) return cb(new Error("no language present"));
-        spundle(options.localeRoot, country, language, iferr(cb, function (messages) {
-            cb(null, messages[[language, country].join('-')][bundle]);
+        lookupContent(country, language, iferr(cb, function (messages) {
+            cb(null, messages[bundle]);
         }));
     }).registerWith(dust);
+
     message.registerWith(dust);
 
+
+    function defaultContent(name, context, cb) {
+        /* Handle paypal-style or bcp47-style objects */
+        var country = context.get('context.locality.country') || context.get('context.locality.langtag.region');
+        var language = context.get('context.locality.language') || context.get('context.locality.langtag.language.language');
+
+        lookupContent(country, language, iferr(cb, function (messages) {
+            cb(null, {context: { "intl": {"messages": messages[context.templateName + '.properties'] } }});
+        }));
+    }
+
+    function lookupContent(country, language, cb) {
+        if (!country ) return cb(new Error("no country present"));
+        if (!language) return cb(new Error("no language present"));
+
+        spundle(options.localeRoot, country, language, iferr(cb, function (messages) {
+            cb(null, messages[[language, country].join('-')]);
+        }));
+    }
 };
 
 module.exports.registerWith = module.exports;
