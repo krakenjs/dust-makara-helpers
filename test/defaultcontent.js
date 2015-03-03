73b6a7c0ee192e422e1c629d536bda7da2ba650a
Aria Stewart <aredridel@dinhe.net>
2015-03-03 08:23:42 -0500

    Add tests

---
+++ test/defaultcontent.js
@@ -0,0 +1,20 @@
+"use strict";
+var dust = require('dustjs-linkedin');
+var makarahelpers = require('../');
+var spundle = require('spundle');
+var test = require('tape');
+var path = require('path');
+
+test('apply default content', function (t) {
+    makarahelpers.registerWith(dust, {localeRoot: path.resolve(__dirname, 'fixtures')});
+
+    dust.addLoadMiddleware(function (name, context, cb) {
+        cb(null, '{@message key="hello"/} {@useContent bundle="world.properties"}{@message key="world" /}{/useContent}');
+    });
+
+    dust.render('test', {context: {locality: { country: 'US', language: 'en' } } }, function (err, out) {
+        t.error(err);
+        t.equal(out, 'Hello, World');
+        t.end();
+    });
+});
