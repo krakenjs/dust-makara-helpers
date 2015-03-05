bb661d06b37897465d2b49476586a39a548d460d
Aria Stewart <aredridel@dinhe.net>
2015-03-05 09:55:10 -0500

    Add failing test for localization with layouts

--- test/defaultcontent.js
+++ test/defaultcontent.js
@@ -1,16 +1,42 @@
 "use strict";
-var dust = require('dustjs-linkedin');
 var makarahelpers = require('../');
 var spundle = require('spundle');
 var test = require('tape');
 var path = require('path');
+var freshy = require('freshy').freshy;
 
 test('apply default content', function (t) {
+    var dust = freshy('dustjs-linkedin');
+
     makarahelpers.registerWith(dust, {localeRoot: path.resolve(__dirname, 'fixtures')});
 
     dust.addLoadMiddleware(function (name, context, cb) {
         cb(null, '{@message key="hello"/} {@useContent bundle="world.properties"}{@message key="world" /}{/useContent}');
     });
+
+
+
+    dust.render('test', {locale: { country: 'US', language: 'en' } }, function (err, out) {
+        t.error(err);
+        t.equal(out, 'Hello, World');
+        t.end();
+    });
+});
+
+test('apply default content when layout present', function (t) {
+    var dust = freshy('dustjs-linkedin');
+
+    makarahelpers.registerWith(dust, {localeRoot: path.resolve(__dirname, 'fixtures')});
+
+    dust.addLoadMiddleware(function (name, context, cb) {
+        if (name == 'test') {
+            cb(null, '{>"layout" /}{<body}{@message key="hello"/} {@useContent bundle="world.properties"}{@message key="world" /}{/useContent}{/body}');
+        } else {
+            cb(null, '{+body/}');
+        }
+    });
+
+
 
     dust.render('test', {locale: { country: 'US', language: 'en' } }, function (err, out) {
         t.error(err);
